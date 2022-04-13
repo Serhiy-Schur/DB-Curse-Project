d2459dac8ed310df81f396d36175004fd8d42c2c
S3rG0 <chur201456@gmail.com>
2022-04-14 00:12:21 +0300

    Додав таблиці Пасажира

--- database.js
+++ database.js
@@ -7,6 +7,15 @@ const conn = mysql2.createConnection({
     password: '12345',
     database: 'kursach'
 })
+
+///Ann connection
+//     host: '25.22.131.112',
+//     port: '3306',
+//     user: 'sergo',
+//     password: '12345',
+//     database: 'kursach'
+
+
 conn.connect(err => {
     if (err) {
         console.log(err);
@@ -16,6 +25,21 @@ conn.connect(err => {
         console.log('Connected OK');
     }
 })
+// const createDatabaseAdmin = () => Promise.all([
+//     createEkipazhTable(),
+//     createAviakompaniyaTable(),
+//     createPasazhyrTable(),
+//     createBiletTable(),
+//     createRaysTable(),
+//     createAeroportTable(),
+//     createMarshrutTable(),
+//     createRaysTable()
+// ]);
+
+
+
+
+
 
 // let querry="SELECT * FROM kursach.екіпаж_адміністратор WHERE екіпаж_адміністратор.кількість_кухарів = 4 AND екіпаж_адміністратор.клас = 2";
 //
@@ -33,7 +57,7 @@ conn.connect(err => {
 //     // console.log(field);
 // });
 
-///Створення таблиці Екіпаж_Адміністратор
+// ///Створення таблиці Екіпаж_Адміністратор
 // const createEkipazhTable = () => conn.query(
 //     `CREATE TABLE IF NOT EXISTS Екіпаж_Адміністратор (
 //           Номер_екіпажу INT NOT NULL AUTO_INCREMENT,
@@ -43,8 +67,8 @@ conn.connect(err => {
 //           Кількість_бортпровідників INT NOT NULL,
 //           PRIMARY KEY (Номер_екіпажу),
 //           UNIQUE INDEX id_UNIQUE (Номер_екіпажу ASC) VISIBLE)`);
-
-///Створення таблиці Авіакомпанія_Адміністратор
+//
+// ///Створення таблиці Авіакомпанія_Адміністратор
 // const createAviakompaniyaTable = () =>conn.query(
 //     `CREATE TABLE IF NOT EXISTS Авіакомпанія_Адміністратор (
 //           Номер_авіакомпанії INT NOT NULL AUTO_INCREMENT,
@@ -54,8 +78,8 @@ conn.connect(err => {
 //           Контактний_номер_телефону INT NOT NULL,
 //           PRIMARY KEY (Номер_авіакомпанії),
 //           UNIQUE INDEX id_UNIQUE (Номер_авіакомпанії ASC) VISIBLE)`);
-
-///Створення таблиці Аеропорт_Адміністратор
+//
+// ///Створення таблиці Аеропорт_Адміністратор
 // const createAeroportTable = () =>conn.query(
 //     `CREATE TABLE IF NOT EXISTS Аеропорт_Адміністратор (
 //           Назва_аеропорту INT NOT NULL AUTO_INCREMENT,
@@ -65,8 +89,8 @@ conn.connect(err => {
 //           Кількість_терміналів INT NOT NULL,
 //           PRIMARY KEY (Назва_аеропорту),
 //           UNIQUE INDEX id_UNIQUE (Назва_аеропорту ASC) VISIBLE)`);
-
-///Створення таблиці Маршрут_Адміністратор
+//
+// ///Створення таблиці Маршрут_Адміністратор
 // const createMarshrutTable = () =>conn.query(
 //     `CREATE TABLE IF NOT EXISTS Маршрут_Адміністратор (
 //           Номер_маршруту INT NOT NULL AUTO_INCREMENT,
@@ -76,8 +100,8 @@ conn.connect(err => {
 //           Статус_маршруту VARCHAR(70) NOT NULL,
 //           PRIMARY KEY (Номер_маршруту),
 //           UNIQUE INDEX id_UNIQUE (Номер_маршруту ASC) VISIBLE)`);
-
-///Створення таблиці Пасажир_Адміністратор
+//
+// ///Створення таблиці Пасажир_Адміністратор
 // const createPasazhyrTable = () =>conn.query(
 //     `CREATE TABLE IF NOT EXISTS Пасажир_Адміністратор (
 //           Номер_пасажира INT NOT NULL AUTO_INCREMENT,
@@ -88,8 +112,8 @@ conn.connect(err => {
 //           Номер_телефону_пасажира INT NOT NULL,
 //           PRIMARY KEY (Номер_пасажира),
 //           UNIQUE INDEX id_UNIQUE (Номер_пасажира ASC) VISIBLE)`);
-
-///Створення таблиці Рейс_Адміністратор
+//
+// ///Створення таблиці Рейс_Адміністратор
 // const createRaysTable = () =>conn.query(
 //     `CREATE TABLE IF NOT EXISTS Рейс_Адміністратор (
 //           Номер_рейсу INT NOT NULL AUTO_INCREMENT,
@@ -98,8 +122,8 @@ conn.connect(err => {
 //           Час_в_шляху INT NOT NULL,
 //           PRIMARY KEY (Номер_рейсу),
 //           UNIQUE INDEX id_UNIQUE (Номер_рейсу ASC) VISIBLE)`);
-
-///Створення таблиці Білет_Адміністратор
+//
+// ///Створення таблиці Білет_Адміністратор
 // const createBiletTable = () =>conn.query(
 //     `CREATE TABLE IF NOT EXISTS Білет_Адміністратор (
 //           Номер_білета INT NOT NULL AUTO_INCREMENT,
@@ -107,8 +131,8 @@ conn.connect(err => {
 //           Ціна INT NOT NULL,
 //           PRIMARY KEY (Номер_білета),
 //           UNIQUE INDEX id_UNIQUE (Номер_білета ASC) VISIBLE)`);
-
-///Створення таблиці Літак_Адміністратор
+//
+// ///Створення таблиці Літак_Адміністратор
 // const createRaysTable = () =>conn.query(
 //     `CREATE TABLE IF NOT EXISTS Літак_Адміністратор (
 //           Номер_літака INT NOT NULL AUTO_INCREMENT,
@@ -117,3 +141,90 @@ conn.connect(err => {
 //           Тип_літака VARCHAR(70) NOT NULL,
 //           PRIMARY KEY (Номер_літака),
 //           UNIQUE INDEX id_UNIQUE (Номер_літака ASC) VISIBLE)`);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ///Створення таблиці Екіпаж_Пасажир
+// const createEkipazhTable = () => conn.query(
+//     `CREATE TABLE IF NOT EXISTS Екіпаж_Пасажир (
+//           ПІБ VARCHAR(100) NOT NULL,
+//           Посада VARCHAR(70) NOT NULL,
+//           Кількість_вильотів INT NOT NULL,
+//           Стаж INT NOT NULL)`);
+//
+// ///Створення таблиці Авіатермінали_Пасажир
+// const createAviaterminalTable = () =>conn.query(
+//     `CREATE TABLE IF NOT EXISTS Авіатермінали_Пасажир (
+//           Індентифікатор INT NOT NULL,
+//           Оператор VARCHAR(70) NOT NULL,
+//           Стійки_реєстраціїї INT NOT NULL,
+//           Стійки_паспортного_контроля INT NOT NULL)`);
+//
+// ///Створення таблиці Аеропорт_Пасажир
+// const createAeroportTable = () =>conn.query(
+//     `CREATE TABLE IF NOT EXISTS Аеропорт_Пасажир (
+//           Назва INT NOT NULL,
+//           Країна VARCHAR(70) NOT NULL,
+//           Адреса INT NOT NULL,
+//           Кількість_літаків INT NOT NULL,
+//           Кількість_персоналу INT NOT NULL)`);
+//
+// ///Створення таблиці Тарифи для пасажирів_Пасажир
+// const createTaryfPasazhyrTable = () =>conn.query(
+//     `CREATE TABLE IF NOT EXISTS Тарифи_для_пасажирів_Пасажир (
+//           Назва_тарифу_для_пасажирів INT NOT NULL,
+//           Багаж INT NOT NULL,
+//           Ручна_поклажа INT NOT NULL,
+//           Ціна INT NOT NULL,
+//           Вибір_місця VARCHAR(70) NOT NULL,
+//           Зміна_бронювання VARCHAR(70) NOT NULL)`);
+//
+// ///Створення таблиці Тарифи на перевезення тварин_Пасажир
+// const createTaryfTvarynaTable = () =>conn.query(
+//     `CREATE TABLE IF NOT EXISTS Тарифи_на_перевезення_тварин_Пасажир (
+//           Назва_тарифу_для_тварин INT NOT NULL,
+//           Тварина VARCHAR(70) NOT NULL,
+//           Ціна INT NOT NULL,
+//           Габарити(в*ш*г) INT NOT NULL,
+//           Вага(кг) INT NOT NULL)`);
+
+///Створення таблиці Рейс_Пасажир
+// const createRaysTable = () =>conn.query(
+//     `CREATE TABLE IF NOT EXISTS Рейс_Пасажир (
+//           Номер_рейсу INT NOT NULL,
+//           Час_вильоту INT NOT NULL,
+//           Дата_вильоту DATE NOT NULL,
+//           Дата_прибуття DATE NOT NULL,
+//           Час_прибуття INT NOT NULL)`);
+//
+// ///Створення таблиці Квиток_Пасажир
+// const createKvytokTable = () =>conn.query(
+//     `CREATE TABLE IF NOT EXISTS Квиток_Пасажир (
+//           Індифікатор_квитка INT NOT NULL,
+//           Дата_продажу DATE NOT NULL,
+//           Ціна INT NOT NULL,
+//           ПІБ_касира VARCHAR(100) NOT NULL,
+//           Місце INT NOT NULL)`);
+//
+// ///Створення таблиці Літак_Пасажир
+// const createLitakTable = () =>conn.query(
+//     `CREATE TABLE IF NOT EXISTS Літак_Пасажир (
+//           Номер INT NOT NULL,
+//           Марка VARCHAR(70) NOT NULL,
+//           Модель INT NOT NULL,
+//           Дата_випуску DATE NOT NULL,
+//           Місткість_багажу INT NOT NULL,
+//           Кількість_пасажирів INT NOT NULL)`);
+
+// module.exports = {
+//     createDatabaseAdmin
+// }
+// createRaysTable();
+// createAviaterminalTable();
+// createTaryfPasazhyrTable();
+// createTaryfTvarynaTable();
+// createAeroportTable();
+// createEkipazhTable();
+// createLitakTable();
+// createKvytokTable();
