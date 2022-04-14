819a302f265c4d923a32a2e1aee2bd3cb761843c
S3rG0 <chur201456@gmail.com>
2022-04-15 01:36:48 +0300

    Зробив всі сторінки Адміна

--- app.js
+++ app.js
@@ -26,29 +26,46 @@ app.get("/", function(req, res){
     });
 });
 
-//запит 1
-app.get("/create", urlencodedParser, function(req, res){
-    pool.query("SELECT * FROM літак_адміністратор", function(err, data) {
-        if(err) return console.log(err);
-        res.render("create.hbs", {
-            users: data
-        });
-    });
-});
+// //запит 1
+// app.get("/create", urlencodedParser, function(req, res){
+//     pool.query("SELECT * FROM літак_адміністратор", function(err, data) {
+//         if(err) return console.log(err);
+//         res.render("create.hbs", {
+//             users: data
+//         });
+//     });
+// });
+//
+// app.get("/create/:value", urlencodedParser, function(req, res){
+//     const value = req.params.value;
+//     pool.query("SELECT * FROM літак_адміністратор WHERE Марка_літака=?",[value], function(err, data) {
+//         if(err) return console.log(err);
+//         res.render("create.hbs", {
+//             users: data
+//         });
+//
+//     });
+// });
+// //запит 2
+//
+// //запит 3
+// app.get("/create/:value/:value2", urlencodedParser, function(req, res){
+//     const value = req.params['value'];
+//     console.log(value);
+//     const value2 = req.params['value2'];
+//     pool.query("SELECT * FROM літак_адміністратор WHERE Марка_літака=? AND Номер_літака=?",[value,value2], function(err, data) {
+//         if(err) return console.log(err);
+//         res.render("create.hbs", {
+//             users: data
+//         });
+//     });
+// });
 
-app.get("/create/:value", urlencodedParser, function(req, res){
-    const value = req.params.value;
-    pool.query("SELECT * FROM літак_адміністратор WHERE Марка_літака=?",[value], function(err, data) {
-        if(err) return console.log(err);
-        res.render("create.hbs", {
-            users: data
-        });
 
-    });
-});
-//запит 2
+//////////////////////// Запити Аднімістратор
+//Запит Екіпаж
 app.get("/Ekipag_A", function (req, res) {
-    pool.query("SELECT * FROM літак_адміністратор", function(err, data) {
+    pool.query("SELECT * FROM екіпаж_адміністратор", function(err, data) {
         if(err) return console.log(err);
         res.render("Ekipag_A.hbs", {
             users: data
@@ -56,8 +73,6 @@ app.get("/Ekipag_A", function (req, res) {
     });
 });
 app.get("/Ekipag_A/:value/:value2", urlencodedParser, function(req, res){
-    //SELECT Номер_екіпажу, Кількість_пілотів, Кількість_бортпровідників FROM kursach.екіпаж_адміністратор WHERE  Кількість_кухарів > ? AND Клас =?;
-
     const value = req.params['value'];
     console.log(value);
     const value2 = req.params['value2'];
@@ -68,21 +83,6 @@ app.get("/Ekipag_A/:value/:value2", urlencodedParser, function(req, res){
         });
     });
 });
-//запит 3
-app.get("/create/:value/:value2", urlencodedParser, function(req, res){
-    const value = req.params['value'];
-    console.log(value);
-    const value2 = req.params['value2'];
-    pool.query("SELECT * FROM літак_адміністратор WHERE Марка_літака=? AND Номер_літака=?",[value,value2], function(err, data) {
-        if(err) return console.log(err);
-        res.render("create.hbs", {
-            users: data
-        });
-    });
-});
-
-
-//////////////////////// Запити Аднімістратор
 
 //Запит Літак
 app.get("/Litak_A", function (req, res) {
@@ -94,8 +94,6 @@ app.get("/Litak_A", function (req, res) {
     });
 });
 app.get("/Litak_A/:value/:value2", urlencodedParser, function(req, res){
-    //SELECT Тип_літака, Номер_літака, Модель_літака FROM kursach.літак_адміністратор WHERE Марка_літака =? AND Авіакомпанія_власник =?
-
     const value = req.params['value'];
     console.log(value);
     const value2 = req.params['value2'];
@@ -107,6 +105,133 @@ app.get("/Litak_A/:value/:value2", urlencodedParser, function(req, res){
     });
 });
 
+//Запит Пасажир
+app.get("/Pasazhyr_A", function (req, res) {
+    pool.query("SELECT * FROM пасажир_адміністратор", function(err, data) {
+        if(err) return console.log(err);
+        res.render("Pasazhyr_A.hbs", {
+            users: data
+        });
+    });
+});
+app.get("/Pasazhyr_A/:value/:value2", urlencodedParser, function(req, res){
+    const value = req.params['value'];
+    console.log(value);
+    const value2 = req.params['value2'];
+    pool.query("SELECT * FROM пасажир_адміністратор WHERE Стать =? AND Країна =?",[value,value2], function(err, data) {
+        if(err) return console.log(err);
+        res.render("Pasazhyr_A.hbs", {
+            users: data
+        });
+    });
+});
+
+//Запит Білет
+app.get("/Bilet_A", function (req, res) {
+    pool.query("SELECT * FROM білет_адміністратор", function(err, data) {
+        if(err) return console.log(err);
+        res.render("Bilet_A.hbs", {
+            users: data
+        });
+    });
+});
+app.get("/Bilet_A/:value/:value2", urlencodedParser, function(req, res){
+    const value = req.params['value'];
+    console.log(value);
+    const value2 = req.params['value2'];
+    pool.query("SELECT * FROM білет_адміністратор WHERE Дата_продажу =? AND Рейс =?",[value,value2], function(err, data) {
+        if(err) return console.log(err);
+        res.render("Bilet_A.hbs", {
+            users: data
+        });
+    });
+});
+
+//Запит Авіакомпанія
+app.get("/Aviakompaniya_A", function (req, res) {
+    pool.query("SELECT * FROM авіакомпанія_адміністратор", function(err, data) {
+        if(err) return console.log(err);
+        res.render("Aviakompaniya_A.hbs", {
+            users: data
+        });
+    });
+});
+app.get("/Aviakompaniya_A/:value/:value2", urlencodedParser, function(req, res){
+    const value = req.params['value'];
+    console.log(value);
+    const value2 = req.params['value2'];
+    pool.query("SELECT * FROM авіакомпанія_адміністратор WHERE Контактний_номер_телефону =? AND Назва_авіакомпанії =?",[value,value2], function(err, data) {
+        if(err) return console.log(err);
+        res.render("Aviakompaniya_A.hbs", {
+            users: data
+        });
+    });
+});
+
+//Запит Рейс
+app.get("/Rays_A", function (req, res) {
+    pool.query("SELECT * FROM рейс_адміністратор", function(err, data) {
+        if(err) return console.log(err);
+        res.render("Rays_A.hbs", {
+            users: data
+        });
+    });
+});
+app.get("/Rays_A/:value/:value2", urlencodedParser, function(req, res){
+    const value = req.params['value'];
+    console.log(value);
+    const value2 = req.params['value2'];
+    pool.query("SELECT * FROM рейс_адміністратор WHERE Час_в_шляху =? AND Дата_вильоту =?",[value,value2], function(err, data) {
+        if(err) return console.log(err);
+        res.render("Rays_A.hbs", {
+            users: data
+        });
+    });
+});
+
+//Запит Аеропорт
+app.get("/Aeroport_A", function (req, res) {
+    pool.query("SELECT * FROM аеропорт_адміністратор", function(err, data) {
+        if(err) return console.log(err);
+        res.render("Aeroport_A.hbs", {
+            users: data
+        });
+    });
+});
+app.get("/Aeroport_A/:value/:value2", urlencodedParser, function(req, res){
+    const value = req.params['value'];
+    console.log(value);
+    const value2 = req.params['value2'];
+    pool.query("SELECT * FROM аеропорт_адміністратор WHERE Країна_розташування =? AND Назва_аеропорту =?",[value,value2], function(err, data) {
+        if(err) return console.log(err);
+        res.render("Aeroport_A.hbs", {
+            users: data
+        });
+    });
+});
+
+//Запит Маршрут
+app.get("/Marshrut_A", function (req, res) {
+    pool.query("SELECT * FROM маршрут_адміністратор", function(err, data) {
+        if(err) return console.log(err);
+        res.render("Marshrut_A.hbs", {
+            users: data
+        });
+    });
+});
+app.get("/Marshrut_A/:value/:value2", urlencodedParser, function(req, res){
+    const value = req.params['value'];
+    console.log(value);
+    const value2 = req.params['value2'];
+    pool.query("SELECT * FROM маршрут_адміністратор WHERE Довжина <? AND Країна_прибуття =?",[value,value2], function(err, data) {
+        if(err) return console.log(err);
+        res.render("Marshrut_A.hbs", {
+            users: data
+        });
+    });
+});
+
 app.listen(3000, function(){
     console.log("Сервер ожидает подключения...");
 });
+//
